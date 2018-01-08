635275f6e901b6b30ea73df88b6ae17228c2aae9
Mathhman <minecraftmathhman@gmail.com>
2018-01-08 14:29:17 -0500

    Activated JUnit for the repository.

---
+++ src/test/java/edu/woodson/Main.java
@@ -0,0 +1,15 @@
+package edu.woodson;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 1/8/2018
+ */
+public class Main {
+    //methods
+    @Test
+    public static void main(String[] args) {
+    }
+}
