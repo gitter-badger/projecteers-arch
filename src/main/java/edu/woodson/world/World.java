dbf69c86371826305a786055796f5a2eedc9cd33
Mathhman <minecraftmathhman@gmail.com>
2018-01-22 18:37:56 -0500

    EZ

--- src/main/java/edu/woodson/world/World.java
+++ src/main/java/edu/woodson/world/World.java
@@ -3,17 +3,18 @@ package edu.woodson.world;
 import edu.woodson.structure.Structure;
 
 import java.io.File;
+import java.util.List;
 
 public class World {
-    private final Structure structure;
+    private final List<Structure> structure;
     private final File file;
 
-    public World(Structure structure, File file) {
+    public World(List<Structure> structure, File file) {
         this.structure = structure;
         this.file = file;
     }
 
-    public Structure getStructure() {
+    public List<Structure> getStructure() {
         return structure;
     }
 
